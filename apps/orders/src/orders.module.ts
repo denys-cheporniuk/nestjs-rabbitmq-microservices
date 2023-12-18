609dc82c29d7a33f8abc189c9303d1de25b0512b
Denys Cheporniuk <denys.cheporniuk@gmail.com>
2023-12-18 11:40:31 +0200

    setup common lib for apps, add logic for orders app

--- apps/orders/src/orders.module.ts
+++ apps/orders/src/orders.module.ts
@@ -1,10 +1,29 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from "@nestjs/config";
+import { DatabaseModule } from "@app/common";
+import * as Joi from 'joi';
 import { OrdersController } from './orders.controller';
 import { OrdersService } from './orders.service';
+import { OrdersRepository } from "./orders.repository";
+import { MongooseModule } from "@nestjs/mongoose";
+import { Order, OrderSchema } from "./order.schema";
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MONGODB_URI: Joi.string().required(),
+      }),
+      envFilePath: './apps/orders/.env'
+    }),
+    DatabaseModule,
+    MongooseModule.forFeature([{
+      name: Order.name,
+      schema: OrderSchema,
+    }])
+  ],
   controllers: [OrdersController],
-  providers: [OrdersService],
+  providers: [OrdersService, OrdersRepository],
 })
 export class OrdersModule {}
