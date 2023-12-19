84e8971e2d9dcaaeee9b6c41245e164d6c0b55d9
Denys Cheporniuk <denys.cheporniuk@gmail.com>
2023-12-19 16:06:12 +0200

    set up auth service with jwt

--- apps/billing/src/billing.controller.ts
+++ apps/billing/src/billing.controller.ts
@@ -1,10 +1,14 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
-import { BillingService } from './billing.service';
+import { Controller } from '@nestjs/common';
 import { Ctx, EventPattern, Payload, RmqContext } from "@nestjs/microservices";
+import { RmqService } from "@app/common";
+import { BillingService } from './billing.service';
 
 @Controller()
 export class BillingController {
-  constructor(private readonly billingService: BillingService) {}
+  constructor(
+    private readonly billingService: BillingService,
+    private readonly rmqService: RmqService,
+  ) {}
 
   @EventPattern('order_created')
   // @UseGuards(JwtAuthGuard)
